c1bbb167d12bdaf697bd4ce246b0d5375b6388e9
tuanbasketball-2003 <nguyenductuanff2003@gmail.com>
2024-06-09 08:46:52 +0700

    add firebase and display sigin

--- next.config.js
+++ next.config.js
@@ -2,17 +2,17 @@
 const nextConfig = {
   reactStrictMode: true,
   // Fix lỗi Cannot use import outside a module dành cho next 14
-  // transpilePackages: [
-  //   'antd',
-  //   'rc-util',
-  //   '@babel/runtime',
-  //   '@ant-design/icons',
-  //   '@ant-design/icons-svg',
-  //   'rc-pagination',
-  //   'rc-picker',
-  //   'rc-tree',
-  //   'rc-table'
-  // ]
+  transpilePackages: [
+    'antd',
+    'rc-util',
+    // '@babel/runtime',
+    '@ant-design/icons',
+    '@ant-design/icons-svg',
+    'rc-pagination',
+    'rc-picker',
+    'rc-tree',
+    'rc-table'
+  ]
 }
 
 module.exports = nextConfig
--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,7 +1,65 @@
-import React from "react";
-
+import { collectionNames } from "@/constants/collectionNames";
+import { firebase } from "@/firebases/firebaseConfig";
+import { UserModel } from "@/models/UserModel";
+import { Button, Space } from "antd";
+import Table, { ColumnProps } from "antd/es/table";
+import { collection, onSnapshot } from "firebase/firestore";
+import React, { use, useEffect, useState } from "react";
+import { BiTrash } from "react-icons/bi";
 const Users = () => {
-  return <div>Users</div>;
+  const [users, setUsers] = useState<UserModel[]>([]);
+
+  useEffect(() => {
+    onSnapshot(collection(firebase, collectionNames.users), (snap) => {
+      if (snap.empty) {
+        console.log("Data is empty");
+      } else {
+        const items: any = [];
+
+        snap.forEach((item: any) =>
+          items.push({
+            id: item.id,
+            ...item.data(),
+          })
+        );
+
+        setUsers(items);
+      }
+    });
+  }, []);
+
+  const colums: ColumnProps<UserModel>[] = [
+    {
+      key: "Name",
+      dataIndex: "displayName",
+      title: "User name",
+    },
+    {
+      key: "email",
+      dataIndex: "email",
+      title: "Email",
+    },
+    {
+      key: "createdAt",
+      dataIndex: "creationTime",
+      title: "Sign up",
+      render: (val: Date) => new Date(val).toISOString(),
+      align: "center",
+    },
+    {
+      key: "btn",
+      title: "",
+      dataIndex: "",
+      render: (item: UserModel) => (
+        <Space>
+          <Button icon={<BiTrash size={20} />} danger type="text" />
+        </Space>
+      ),
+      align: "center",
+    },
+  ];
+
+  return <Table dataSource={users} columns={colums}></Table>;
 };
 
 export default Users;
